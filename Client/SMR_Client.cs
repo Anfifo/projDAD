0f1409207c203a4f0aa289fee2a0cc52821f4777
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2018-11-06 19:09:29 +0000

    bc2ebcu2e

---
+++ Client/SMR_Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTypes;
+
+namespace Client
+{
+    class SMR_Client : ITSpaceAPI
+    {
+        private string[] view;
+        public string[] GetView()
+        {
+            return view;
+        }
+
+        public void Put(ITuple tuple)
+        {
+            // Send total order multicast to all members of the view
+
+            throw new NotImplementedException();
+        }
+
+        public ITuple Read(ITuple template)
+        {
+            // Send total order multicast to all members of the view
+
+            // Return after the first replica answers
+
+            throw new NotImplementedException();
+        }
+
+        public ITuple Take(ITuple template)
+        {
+            // Send total order multicast to all members of the view
+
+            // Select one tuple from the subset the first replica returns
+
+            // Send total order multicast to all members of the view with deletion request
+
+            throw new NotImplementedException();
+        }
+
+        public void UpdateView(string[] group)
+        {
+            this.view = group;
+        }
+    }
+}
---
+++ Client/TO_Multicast.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTypes;
+
+namespace Client
+{
+    class TO_Multicast
+    {
+
+        /// <summary>
+        /// Largest agreed sequence number it has seen in the view.
+        /// </summary>
+        private int agreedSeq;
+
+        /// <summary>
+        /// Largest proposed sequence number. 
+        /// </summary>
+        private int proposedSeq;
+
+        /// <summary>
+        /// Group of processes in the view.
+        /// </summary>
+        private string[] view;
+
+        /// <summary>
+        /// Number of the current view.
+        /// </summary>
+        private int viewNr;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="view">Group of processes in the view.</param>
+        /// <param name="viewNr">View number.</param>
+        public TO_Multicast(string[] view, int viewNr)
+        {
+            this.viewNr = viewNr;
+            this.view = view;
+        }
+
+        /// <summary>
+        /// Getter and setter for the view.
+        /// </summary>
+        public string[] View
+        {
+            get { return view; }
+            set { view = value;  }
+        }
+
+        /// <summary>
+        /// Getter and setter for the view number.
+        /// </summary>
+        public int ViewNr
+        {
+            get { return viewNr;  }
+            set { viewNr = value;  }
+        }
+        
+        /// <summary>
+        /// Multicast send with total order.
+        /// </summary>
+        /// <param name="message">message to send</param>
+        public void Send(ITSpaceRequest message)
+        {
+            // Multicast send <message, i>.
+
+
+            // Repeat until all processes in the view have answered.
+
+
+            // Select A = largest proposed sequence number.
+
+
+            // Multicast send <i, A>.
+
+
+            // Repeat until all processes have acknowledged.
+
+        }
+
+ 
+    }
+}
