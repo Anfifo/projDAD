49d039da78e21be99dcd857c7d1a71e261db14d2
Anfifo <afonseca-1995@hotmail.com>
2018-12-01 23:22:04 +0000

    added SRM State

---
+++ Server/SMRState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommonTypes;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Server
+{
+    /// <summary>
+    /// Class representing a "View" on a server or client
+    /// A view is a list of the current alive servers 
+    /// </summary>
+    [Serializable]
+    public class SMRState
+    {
+        
+        public static List<string> ProcessedRequests { get => ProcessedRequests; set => ProcessedRequests = value; }
+        public int SequenceNumber { get => SequenceNumber; set => SequenceNumber = value; }
+        public View ServerView { get => ServerView; set => ServerView = value; }
+        public List<Message> MessageQueue { get => MessageQueue; set => MessageQueue = value; }
+    }
+}
